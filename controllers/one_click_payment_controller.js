e26668a044eb9254d111849c0b784dd256acb75f
fadhelma <128975299+fadhelma@users.noreply.github.com>
2023-06-03 13:12:45 +0300

    solve crash

--- controllers/one_click_payment_controller.js
+++ controllers/one_click_payment_controller.js
@@ -9,34 +9,38 @@ const api = process.env.API;
 
 const oneClickPaymentController = async (req, res)  =>  {
     const localCard = store.get('card');
-
-    const path = '/v1/checkouts';
-    const data = querystring.stringify({
-      'entityId': localCard.cardBrand == "mada" ? entityIds[0] : entityIds[1],
-      'amount':'1.00',
-      'currency': 'SAR',
-      'paymentType': 'DB',
-      'registrations[0].id':localCard.cardId
-    });
-  
-    const options = {
-      port: 443,
-      host: api,
-      path: path,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': data.length,
-        'Authorization': `Bearer ${accessToken}`
+    if(localCard == null){
+      res.send('You have no saved cards');
+    }else{
+      const path = '/v1/checkouts';
+      const data = querystring.stringify({
+        'entityId': localCard.cardBrand == "mada" ? entityIds[0] : entityIds[1],
+        'amount':'1.00',
+        'currency': 'SAR',
+        'paymentType': 'DB',
+        'registrations[0].id':localCard.cardId
+      });
+    
+      const options = {
+        port: 443,
+        host: api,
+        path: path,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': data.length,
+          'Authorization': `Bearer ${accessToken}`
+        }
+      };
+    
+      try {
+        const result = await sendRequest(options, data);
+        res.render('one_click_payment_view', { result, paymentMethod:localCard.cardBrand });
+      } catch (error) {
+        res.status(500).json({ error: error.message });
       }
-    };
-  
-    try {
-      const result = await sendRequest(options, data);
-      res.render('one_click_payment_view', { result, paymentMethod:localCard.cardBrand });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
     }
+
 };
 
 module.exports = oneClickPaymentController;
